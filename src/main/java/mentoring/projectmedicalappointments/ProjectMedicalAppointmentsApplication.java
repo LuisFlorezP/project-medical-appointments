09d53bf1c06d72b61ee316830f4ca8b67b6b81ab
Luis Ángel Flórez Palacio <luis.florez@pragma.com.co>
2023-12-04 07:49:48 -0500

    Initial commit

---
+++ src/main/java/mentoring/projectmedicalappointments/ProjectMedicalAppointmentsApplication.java
@@ -0,0 +1,13 @@
+package mentoring.projectmedicalappointments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMedicalAppointmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMedicalAppointmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mentoring/projectmedicalappointments/ProjectMedicalAppointmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package mentoring.projectmedicalappointments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMedicalAppointmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
