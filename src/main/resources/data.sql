93f17d737a302d201b5c21f05b8dffea7e1b97b0
Luis Ángel Flórez Palacio <luis.florez@pragma.com.co>
2023-12-04 13:09:10 -0500

    Generate file data.sql.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT IGNORE INTO location_entity (location_id, name) VALUES
+  (1, 'Ciudad de México'),
+  (2, 'Buenos Aires'),
+  (3, 'Madrid'),
+  (4, 'Nueva York'),
+  (5, 'Tokio');
+
+INSERT IGNORE INTO specialization_entity (specialization_id, name) VALUES
+  (1, 'Cardiólogo'),
+  (2, 'Neurólogo'),
+  (3, 'Ginecólogo'),
+  (4, 'Oncólogo'),
+  (5, 'Traumatólogo');
